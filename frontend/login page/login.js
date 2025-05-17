ad0648f44fd38521f27876aac4a08841e5605c99
YukoMenu <yuukomeinu10622@gmail.com>
2025-05-18 00:45:52 +0800

    commit

--- frontend/login page/login.js
+++ frontend/login page/login.js
@@ -78,3 +78,13 @@ signupForm.addEventListener('submit', (e) => {
         window.location.href = '../teacher/index.html';
     }
 });
+
+fetch('http://localhost:3000/users')
+  .then(res => res.json())
+  .then(data => console.log(data));
+
+fetch('http://localhost:3000/users', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ name: 'Alice' })
+});
